9031bec66df25014c79169c86faaf874411c8068
jjinkou2 <jjinkou2@yahoo.fr>
2013-02-22 00:37:03 +0100

    jsonfinal

--- XL2JSON.hs
+++ XL2JSON.hs
@@ -3,9 +3,8 @@
 
 import ExcelCom 
 import RawToJSON
-import KpiStructure
 import qualified Data.Text as T 
-import Data.Aeson.Types (Pair)
+import Data.Aeson.Types (Pair,Value)
 import Data.Aeson
 import qualified Data.ByteString.Lazy.Char8 as BL
 
@@ -16,32 +15,17 @@ fichierTest3 = "E:/Programmation/haskell/Com/qos.xls"
 fichierTest4 = "C:/Users/lyup7323/Developpement/Haskell/Com/qos.xls"
 
 
-instance ToJSON KpiStruct where
-    toJSON KpiStruct{..} = object [kpiName .= kpiData]
-
-instance ToJSON ServStruct where
-    toJSON ServStruct{..} = object [servName .= kpis]
-
-
 
 sheetsName = ["BIV","BIC"]
-
-main1 = coRun $ do
-    (pExl, workBooks, workSheets) <- xlInit
-    mapM_ (processRowData' workSheets) sheetsName
-    xlQuit workBooks pExl
 
 
 servToBS :: [Pair] -> BL.ByteString 
 servToBS  = encode . object 
-servToBS' :: Services -> BL.ByteString 
-servToBS'  = encode . map toJSON
 
 main = coRun $ do 
     (pExl, workBooks, workSheets) <- xlInit
-    xs <- mapM (processRowData'' workSheets) sheetsName
-    BL.writeFile "json.txt" $ servToBS' xs     
-    
+    xs <- mapM (processRowData workSheets) sheetsName
+    BL.writeFile "json.txt" $ servToBS xs     
 
     xlQuit workBooks pExl
 
@@ -63,21 +47,12 @@ xlInit = do
 
 processRowData :: Sheet a -> String -> IO Pair
 processRowData sheets sheetName = do 
-    rowsService <- rowsFromSheet sheets sheetName
-    return $ servToPair rowsService (T.pack sheetName) 
-    
-processRowData'' :: Sheet a -> String -> IO ServStruct
-processRowData'' sheets sheetName= do 
-    rowsService <- rowsFromSheet sheets sheetName
-    return $ ServStruct (T.pack sheetName) (rawToStruct rowsService)
-    
-processRowData' :: Sheet a -> String -> IO ()
-processRowData' sheets sheetName = do 
-    rowsService <- rowsFromSheet sheets sheetName
     putStrLn $ "got all datas from " ++ sheetName
-    printListData rowsService
     putStrLn $ replicate 50 '-'
-
+    rowsService <- rowsFromSheet sheets sheetName
+    return $ servToPair rowsService (T.pack sheetName)  
+    
+    
 
 rowsFromSheet :: Sheet a -> String -> IO [String] 
 rowsFromSheet workSheets sheetName= do 
